74af183e5ed17b260847a3aec7748b5f2dbb22c1
Simon Hoyos <shmesa23@gmail.com>
2021-02-17 10:23:59 -0500

    Create multiply function (#3)

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function sum(num1, num2) {
   console.log('hola mundo')
+  console.log('chao mundo')
   return num1 + num2
+}
+
+function multiply(a, b) {
+  return a * b
 }
 
 function sub(num1, num2) {
